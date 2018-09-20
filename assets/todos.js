d224de0723da0b605dbca837421523059aaa45db
i-vishi <rr.vishalgaur@gmail.com>
2018-09-20 20:59:40 +0530

    add all required files

---
+++ assets/todos.js
@@ -0,0 +1,27 @@
+// check off specific Todos by clicking
+$("ul").on("click", "li", function(){
+	$(this).toggleClass("completed");
+});
+
+//click on X to delete
+$("ul").on("click", "span", function(event){
+	$(this).parent().fadeOut(500, function(){
+		$(this).remove();
+	});
+	event.stopPropagation();
+});
+
+// adding new todos
+$("input[type='text']").keypress(function(event){
+	if(event.which === 13){
+		//grab new todo from input tag
+		var todoText = $(this).val();
+		$(this).val("");
+		//create neew li and add to ul
+		$("ul").append("<li><span><i class='fas fa-trash-alt'></i></span> " + todoText + "</li>")
+	}
+});
+
+$(".fa-plus").click(function(){
+	$("input[type='text']").fadeToggle();
+});
